3ea8c496866e6ed43b7714f21888d1b90adaf090
Alex Huddleston <adh9694@gmail.com>
2018-12-22 19:15:47 -0600

    selenium tested and working.

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,13 @@
+from selenium import webdriver
+import time
+
 print('hello, world!')
 print('commit test.')
+
+browser=webdriver.Firefox()
+
+browser.get("https://google.com")
+
+time.sleep(5)
+
+browser.quit()
