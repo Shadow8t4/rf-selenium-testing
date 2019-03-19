84825a7884ff6fce8e1d3fed9950939ff1be2b09
Alex Huddleston <adh9694@gmail.com>
2019-03-19 18:49:01 -0500

    Can't get selenium to work in vscode atm oh well

--- rf_debugger.py
+++ rf_debugger.py
@@ -1,12 +1,9 @@
 from subprocess import call
-from time import sleep
 from os import getcwd
-import sys
+from sys import argv
 
 def main():
-    # Split into a thread, then run robot test.
-    call('java -jar {0}/selenium-server-standalone.jar'.format(getcwd()).split())
-    sleep(3)
-    call(["robot", str(sys.argv[1])])
+    call('{0}/.pyenv/bin/python -m robot {1}'.format(getcwd(), str(argv[1])).split())
+    return 0
 
 main()
