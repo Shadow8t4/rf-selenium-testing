7364a6e582d74c5effc326b12721aef5706f206c
Alex Huddleston <adh9694@gmail.com>
2019-03-19 01:26:47 -0500

    Small edits. Trying to make work in vscode

--- rf_debugger.py
+++ rf_debugger.py
@@ -1,7 +1,12 @@
 from subprocess import call
+from time import sleep
+from os import getcwd
 import sys
 
 def main():
+    # Split into a thread, then run robot test.
+    call('java -jar {0}/selenium-server-standalone.jar'.format(getcwd()).split())
+    sleep(3)
     call(["robot", str(sys.argv[1])])
 
 main()
--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
+# Manual activation
 
 if [ ! -d "report" ]; then
     mkdir report
 fi
 
-export PATH=$PATH:$(pwd)/
 echo $PATH
 java -jar selenium-server-standalone.jar &
 sleep 5
 robot -d report test.robot
-kill $!
+killall java
